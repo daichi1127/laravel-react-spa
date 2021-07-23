0974addd31f0bb9ef07c2d2e2b7611c022c9a2fd
Daichi Murakami <14b1205@gmail.com>
2021-07-23 22:04:51 +0900

    DBのpostsデータをフロント側で取得しステート(posts)にセット

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,6 +18,6 @@ Route::group(['middleware' => 'api'], function(){
     Route::get('posts', 'App\Http\Controllers\Api\PostController@index');
 });
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
