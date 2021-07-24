5b66950f6301a96af421cd8ebe4584976e50e480
Daichi Murakami <14b1205@gmail.com>
2021-07-24 11:01:05 +0900

    新規登録のフロントエンド実装

--- backend/resources/js/pages/Home.js
+++ backend/resources/js/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect }　from 'react';
-import axios from 'axios';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
+import axios from 'axios';
+import PostFrom from '../components/PostFrom';
 
 //スタイルの定義
 const useStyles = makeStyles((theme) => createStyles({
@@ -15,19 +16,6 @@ const useStyles = makeStyles((theme) => createStyles({
 //ヘッダーのコンテンツ用の配列定義
 const headerList = ['名前', 'タスク内容', '編集', '完了'];
 
-let rows = [
-    {
-        name: "モーリー",
-        content: "肩トレ",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },{
-        name: "ドンキーコング",
-        content: "バナナ補給",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },
-];
 
 function Home() {
     //定義したスタイルを利用するための設定
@@ -35,6 +23,9 @@ function Home() {
 
     //postsの状態を管理する
     const [posts, setPosts] = useState([]);
+
+    //フォームの入力値を管理するステートの定義
+    const [formData, setFormData] = useState({name:'', content:''});
 
     //画面に到着したらgetPostsDataを呼ぶ
     useEffect(() => {
@@ -53,6 +44,40 @@ function Home() {
                 console.log('通信に失敗しました');
             });
     }
+
+    //入力がされたら（都度）
+    function inputChange(e) {
+        console.log(e);
+        const key = e.target.name;
+        const value = e.target.value;
+        formData[key] = value;
+        let data = Object.assign({}, formData);
+        setFormData(data);
+    }
+
+    const createPost = async() => {
+        //空だと弾く
+        if(formData == ''){
+            return;
+        }
+        //入力値を投げる
+        await axios
+            .post('/api/post/create', {
+                name: formData.name,
+                content: formData.content
+            })
+            .then((res) => {
+                //戻り値をtodosにセット
+                const tempPosts = posts
+                tempPosts.push(res.data);
+                setPosts(tempPosts)
+                setFormData('');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     //空配列として定義する
     let rows = [];
     //postsの要素ごとにrowsで使える形式に変換する
@@ -69,11 +94,16 @@ function Home() {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-10">
-                    <h1>タスク管理</h1>
+                    <div className="card">
+                        <h1>タスク管理</h1>
+                        <Card className={classes.card}>
+                            <PostFrom data={formData} btnFunc={createPost} inputChange={inputChange} />
+                        </Card>
                         <Card className={classes.card}>
                             {/* テーブル部分の定義 */}
                             <MainTable headerList={headerList} rows={rows} />
                         </Card>
+                    </div>
                 </div>
             </div>
         </div>
